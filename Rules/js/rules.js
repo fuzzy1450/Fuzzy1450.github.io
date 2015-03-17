5226fdc2433fc98168f3a9370cf1264fabe68606
fuzzy1450 <ievank@me.com>
2015-03-16 20:37:09 -0400

    Rule Change
    =Changed Rule 10 in /Rules

--- Rules/js/rules.js
+++ Rules/js/rules.js
@@ -1,4 +1,4 @@
-text = ["Never trust anyone","Always be prepared","Always have a backup plan","A single backup plan will never do, always have a backup backup plan","Always have the bleach ready, as you never know when you will need it","Everyone is guilty until proven innocent","Never let your guard down as everyone is a spy","There is no such thing as safe, only safer","Timing is everything","You never fail, you only learn"];
+text = ["Never trust anyone","Always be prepared","Always have a backup plan","A single backup plan will never do, always have a backup backup plan","Always have the bleach ready, as you never know when you will need it","Everyone is guilty until proven innocent","Never let your guard down as everyone is a spy","There is no such thing as safe, only safer","Timing is everything","Through technicalities, nothing is impossible"];
 images = ["one.png","two.png","three.png","four.png","five.png","six.png","seven.png","ate.png","nine.png","ten.png"];
 
 function addRules(){
