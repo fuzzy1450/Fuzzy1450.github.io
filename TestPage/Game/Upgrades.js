81f6817040420eb145158b1aff6b116c5707a1b3
fuzzy1450 <ievank@me.com>
2015-03-22 00:42:20 -0400

    Game Upgrade Functionality
    =Upgrades now buyable, and other functionality

--- TestPage/Game/Upgrades.js
+++ TestPage/Game/Upgrades.js
@@ -9,7 +9,8 @@ ClickUp1={
 	cost:50,
 	onBuy:"ClickUp1",
 	require:false,
-	icon:"UpgImgs/ClickUp1.svg"
+	icon:"UpgImgs/ClickUp1.svg",
+	bought:userVars.ClickUp1
 }
 
 TicTime1={
@@ -18,7 +19,8 @@ TicTime1={
 	cost:100,
 	onBuy:"TicTime1",
 	require:50,
-	icon:"UpgImgs/TicTime1.svg"
+	icon:"UpgImgs/TicTime1.svg",
+	bought:userVars.TicTime1
 }
 
 
@@ -28,17 +30,53 @@ Upgrades.push(ClickUp1);
 Upgrades.push(TicTime1);
 
 //Added objects to document
-i=0;
 
-while(i!=Upgrades.length){
-	
-	var newDiv=document.createElement('div');
-	newDiv.id="Upgrade";
-	newDiv.title=("Cost: $" + Upgrades[i].cost);
-	document.getElementById('Upgrades').appendChild(newDiv); //add in Upgrade div
 
-	newDiv.innerHTML="<div id='UpgradeOverlay'><img src='" + Upgrades[i].icon + "' id='UpgradeImg'><p id='UpgradeTitle'>" + Upgrades[i].name + "</p><p id='UpgradeDescription'>" + Upgrades[i].description + "</p></div>" //all the magic code. What does any of it mean? I don't know anymore.
-	//TODO add in buy function
+function addUpgrades(){ //needs to be a function so it can be run through multiple times
+	i=0;
+	while(i!=Upgrades.length){
+		
+		if((userVars.maxKaching>=Upgrades[i].require)&&(Upgrades[i].shown!=true)&&(Upgrades[i].bought!=true)){ //if the user has had enough money to display upgrade
+			var newDiv=document.createElement('div');
+			newDiv.className="Upgrade";
+			newDiv.id=Upgrades[i].onBuy;
+			newDiv.title=("Cost: $" + Upgrades[i].cost);
+			document.getElementById('Upgrades').appendChild(newDiv); //add in Upgrade div
+
+			newDiv.innerHTML=("<div id='UpgradeOverlay' onclick=buyUpgrade('" + Upgrades[i].onBuy + "');><img src='" + Upgrades[i].icon + "' id='UpgradeImg'><p id='UpgradeTitle'>" + Upgrades[i].name + "</p><p id='UpgradeDescription'>" + Upgrades[i].description + "</p></div>"); //all the magic code. What does any of it mean? I don't know anymore.
+			Upgrades[i].shown=true;
+		}
 	
-	i++; //increment i
+		i++; //increment i
+	};
+};
+setInterval(addUpgrades, refreshRate); //refresh the upgrades every 2 seconds
+
+function buyUpgrade(name){ //buy an upgrade
+	switch(name){ //the switch is real. I know there is a better way to do this.
+		case "ClickUp1":
+			if(userVars.Kaching>=ClickUp1.cost){
+				userVars.clickIncrement++;
+				userVars.ClickUp1=true;
+				ClickUp1.bought=true;
+				userVars.Kaching-=ClickUp1.cost;
+				var killDiv = document.getElementById("ClickUp1");
+				killDiv.parentNode.removeChild(killDiv); //Div is kill
+			}
+			break;
+			
+		case "TicTime1":
+			if(userVars.Kaching>=TicTime1.cost){
+				userVars.tickTime-=100;
+				userVars.TicTime1=true;
+				TicTime1.bought=true;
+				userVars.Kaching-=TicTime1.cost;
+				var killDiv = document.getElementById("TicTime1");
+				killDiv.parentNode.removeChild(killDiv); //Div is kill
+			}
+			break;
+			
+		default:
+			break;
+	}
 }
--- TestPage/Game/mainGame.js
+++ TestPage/Game/mainGame.js
@@ -3,7 +3,13 @@ userVars ={
 	clickIncrement:1, // Currency gained on click
 	autoIncrement:0, // Currency gained per tick
 	tickTime:2000, // Milliseconds it takes to add AutoIncrement
-	Kaching:0 // Currency
+	Kaching:0, // Currency
+	maxKaching:0, // The highest amount of money held by the player at once
+	
+	
+	//Holy Bought Upgrades, Batman!
+	ClickUp1: false,
+	TicTime1: false
 }
 
 //Permanent Variables
@@ -25,7 +31,11 @@ function saveGame(){ //save all stats as cookies
 	document.cookie=("tickTime=" + userVars.tickTime + ";");
 	document.cookie=("autoIncrement=" + userVars.autoIncrement + ";");
 	document.cookie=("clickIncrement=" + userVars.clickIncrement + ";");
+	document.cookie=("maxKaching=" + userVars.maxKaching + ";");
+	document.cookie=("ClickUp1=" + userVars.ClickUp1 + ";");
+	document.cookie=("TicTime1=" + userVars.TicTime1 + ";");
 	document.cookie=("DoNotChange=true;"); //User has been on before
+	
 }
 setInterval(saveGame, 30000); //Interval to save game to cookies, every 30 seconds
 
@@ -51,5 +61,15 @@ function loadGame(){
 	userVars.tickTime = parseInt(getCookie("tickTime"));
 	userVars.autoIncrement = parseInt(getCookie("autoIncrement"));
 	userVars.clickIncrement = parseInt(getCookie("clickIncrement"));
+	userVars.maxKaching = parseInt(getCookie("maxKaching"));
+	userVars.ClickUp1 = getCookie("ClickUp1")
+	userVars.TicTime1 = getCookie("TicTime1")
 }
-window.onload = checkCookies;
+window.onload = checkCookies
+
+function checkForMax(){ // Check if you should increase the MaxKaching 
+	if(userVars.Kaching>userVars.maxKaching){
+		userVars.maxKaching=userVars.Kaching;
+	}
+}
+setInterval(checkForMax, 750); // Run checkForMax every 0.75 seconds(too quick?)
