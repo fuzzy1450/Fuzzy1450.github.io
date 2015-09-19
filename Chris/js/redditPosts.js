82ab0e41bb9c2400ae90c27b001726e649d49400
fuzzy1450 <ievank@me.com>
2015-09-18 23:16:35 -0400

    Ayy Lmao
    =/Chris is now 50% danker, with picture requests from reddit

---
+++ Chris/js/redditPosts.js
@@ -0,0 +1,87 @@
+var pop;
+var pop2;
+var pop3;
+var pop4;
+
+function restOfScript(){
+	
+pop=pop.concat(pop2);
+pop=pop.concat(pop3);
+pop=pop.concat(pop4);
+
+
+
+
+
+rand=Math.floor(Math.random() * 100);
+var LinkNumb=0;
+var LinkList=document.getElementsByTagName("a");
+var InLoopDone=false;
+while(LinkNumb!=LinkList.length){
+	
+	while(!InLoopDone){
+		
+		rand=Math.floor(Math.random() * 100);
+		if(eval("typeof(num"+rand+")!=\"undefined\"")){
+			//do nuffin
+		} else {
+			
+			
+		if(LinkList[LinkNumb].id!="no-change"){
+			if(pop[rand].data.url[27]!="m"){
+					LinkList[LinkNumb].setAttribute('href', pop[rand].data.url);
+			} else {
+				LinkNumb--;
+			}
+			eval("num"+rand+"=true;");
+		}
+			
+			InLoopDone=true;
+		}
+	}
+	
+	InLoopDone=false;
+	LinkNumb++;
+}
+
+}
+
+$.getJSON("http://www.reddit.com/r/me_irl.json", function(result, status){
+	
+	if(status=="success"){
+	
+		$.each(result, function(i, field){
+			pop=(field.children)
+		});
+	}
+});
+$.getJSON("https://www.reddit.com/r/me_irl.json?count=25", function(result, status){
+	
+	if(status=="success"){
+	
+		$.each(result, function(i, field){
+			pop2=(field.children)
+		});
+	}
+});
+$.getJSON("https://www.reddit.com/r/me_irl.json?count=50", function(result, status){
+	
+	if(status=="success"){
+	
+		$.each(result, function(i, field){
+			pop3=(field.children)
+		});
+	}
+});
+
+$.getJSON("https://www.reddit.com/r/me_irl.json?count=75", function(result, status){
+	
+	if(status=="success"){
+	
+		$.each(result, function(i, field){
+			pop4=(field.children)
+		});
+	}
+});
+window.setTimeout(restOfScript,1000);
+
